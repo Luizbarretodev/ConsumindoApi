7e2cdd4dbf3e6ae8be1b3c1e6db78f7208cd6c90
Luiz Gustavo <gugajoao1020@gmail.com>
2026-05-31 18:26:24 -0300

    primeiro commit

---
+++ AlunosApi/Models/Aluno.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlunosApi.Models
+{
+    //Poderia definir como tabela digitando [Table("Alunos")], mas já definimos no context
+    public class Aluno
+    {
+        //Poderia utilizar o DA [Key], mas o EF entende que ID é uma PK
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(80)]
+        public string Nome { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(80)]
+        public string Email { get; set; }
+
+        [Required]
+        public int Idade { get; set; }
+    }
+}
